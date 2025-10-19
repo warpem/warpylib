a4d31e507e464a7a629a4fe480ef654c786677db
Dimitry Tegunov <tegunov@gmail.com>
2025-10-19 13:03:05 -0700

    Add more ops

--- warpylib/ops/__init__.py
+++ warpylib/ops/__init__.py
@@ -5,5 +5,13 @@ from .rescale import rescale
 from .norm import norm
 from .norm_ft import norm_ft
 from .norm_rft import norm_rft
+from .masking import mask_rectangular
+from .plane import fit_plane, subtract_plane
 
-__all__ = ['resize', 'resize_ft', 'resize_rft', 'rescale', 'norm', 'norm_ft', 'norm_rft']
+__all__ = [
+    'resize', 'resize_ft', 'resize_rft',
+    'rescale',
+    'norm', 'norm_ft', 'norm_rft',
+    'mask_rectangular',
+    'fit_plane', 'subtract_plane'
+]
